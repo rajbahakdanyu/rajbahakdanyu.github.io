82270fbd8d9168c302e743e0ea84a43ac7426684
rajbahakdanyu <rajbahakdanyu@gmail.com>
2026-03-30 11:44:13 +1000

    fix: update favicon link to use SVG format for better scalability

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
